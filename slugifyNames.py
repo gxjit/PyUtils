253aaccc6d39f923ca349e48f4ba6d678c10d85d
Gurjit Singh <g.s@outlook.in>
2022-06-07 19:18:48 +0530

    improves slugifyNames

--- slugifyNames.py
+++ slugifyNames.py
@@ -1,54 +1,107 @@
-import argparse
-import pathlib
 import re
-import unicodedata
+from argparse import ArgumentParser, ArgumentTypeError
+from pathlib import Path
+from unicodedata import normalize
 
 
 def parseArgs():
     def dirPath(pth):
-        pthObj = pathlib.Path(pth)
+        pthObj = Path(pth)
         if pthObj.is_dir():
             return pthObj
         else:
-            raise argparse.ArgumentTypeError("Invalid Directory path")
+            raise ArgumentTypeError("Invalid Directory path")
 
-    parser = argparse.ArgumentParser(description="Slugify filenames.")
+    parser = ArgumentParser(description="Slugify filenames.")
     parser.add_argument("dir", metavar="DirPath", help="Directory path", type=dirPath)
-
+    parser.add_argument(
+        "-u",
+        "--unicode",
+        action="store_true",
+        default=False,
+        help="Allow Unicode.",
+    )
+    parser.add_argument(
+        "-d",
+        "--dots",
+        action="store_true",
+        default=True,
+        help="Keep dots/periods.",
+    )
+    parser.add_argument(
+        "-s",
+        "--spaces",
+        action="store_true",
+        default=True,
+        help="Keep whitespace.",
+    )
+    parser.add_argument(
+        "-c",
+        "--case",
+        action="store_true",
+        default=False,
+        help="Convert to lower case.",
+    )
+    parser.add_argument(
+        "-f",
+        "--files",
+        action="store_true",
+        default=False,
+        help="Process files only/Don't touch directories.",
+    )
     return parser.parse_args()
 
 
-def slugify(value, replace={}, keepSpace=True):
+def slugify(
+    value,
+    allowUnicode=False,
+    keepSpace=True,
+    keepDots=True,
+    lowerCase=False,
+    replace={},  # TODO: extend customizations through cli arguments
+):
     """
     Adapted from django.utils.text.slugify
     https://docs.djangoproject.com/en/3.0/_modules/django/utils/text/#slugify
     """
-    replace.update({"[": "(", "]": ")", ":": "_"})
     value = str(value)
-    value = (
-        unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
-    )
 
+    if allowUnicode:
+        value = normalize("NFKC", value)
+    else:
+        value = normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
+
+    replace = {"[": "(", "]": ")", ":": "_", **replace}  # ".": "_"
     for k, v in replace.items():
         value = value.replace(k, v)
-    value = re.sub(r"[^\w\s)(_-]", "", value).strip()
 
-    if keepSpace:
-        value = re.sub(r"[\s]+", " ", value)
-    else:
+    acceptablePattern = r"[^\w\s)(_-]"
+    # \w word characters/alphanumerics, \s whitespace characters,
+    # underscores, parentheses, and hyphens
+
+    if keepDots:
+        acceptablePattern = acceptablePattern.replace(r"\s", r"\s.")
+
+    value = re.sub(acceptablePattern, "", value).strip()
+
+    if lowerCase:
+        value = value.lower()
+
+    if not keepSpace:
         value = re.sub(r"[-\s]+", "-", value)
     return value
 
 
-getFileList = lambda dirPath: [f for f in dirPath.iterdir() if f.is_file()]
+pargs = parseArgs()
 
-dirPath = parseArgs().dir.resolve()
+dirPath = pargs.dir.resolve()
+
+fileList = list(dirPath.iterdir())
+
+if pargs.files:
+    fileList = [f for f in fileList if f.is_file()]
 
 
-for file in getFileList(dirPath):
-    newName = slugify(file.stem)
-    # print("\n----")
-    # print(file.name)
-    # print(newName)
-    # print("----\n")
-    file.rename(dirPath.joinpath(f"{newName}{file.suffix}"))
+for file in fileList:
+    newName = slugify(file.stem, pargs.unicode, pargs.spaces, pargs.dots, pargs.case)
+    file.rename(file.with_name(newName))
